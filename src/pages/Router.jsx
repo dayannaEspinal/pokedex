82c8672624fd662d2c217f01948d4336194d6d6b
DayannaEspinal <dayannaespinal14@gmail.com>
2026-04-13 11:39:18 -0600

    correccion en route

--- src/pages/Router.jsx
+++ src/pages/Router.jsx
@@ -1,18 +1,19 @@
-import { Routes, Route} from 'react-router';
-import Layout from "./Layout";
-import About from "./About";
-import Catalogo from "./Catalogo";
-import Detail from "./Detail";
+import { Routes, Route } from 'react-router-dom'
+import Layout from './Layout'
+import About from './About'
+import Catalogo from './Catalogo'
+import Detail from './Detail'
+
 const RouterDefinition = () => {
-    return (
-        <Routes>
-            <Route element={<Layout/>}>
-                <Route index element={<Catalogo/>} />
-                <Route path="/about" element={<About/>} />
-                <Route path="/pokemon/:id" element={<Detail />} />
-            </Route>
-        </Routes>
-    );
+  return (
+    <Routes>
+      <Route element={<Layout />}>
+        <Route index element={<Catalogo />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/pokemon/:id" element={<Detail />} />
+      </Route>
+    </Routes>
+  )
 }
 
-export default RouterDefinition;
+export default RouterDefinition
