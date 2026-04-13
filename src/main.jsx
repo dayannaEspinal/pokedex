1fbd188f124b719c67a318e17191fe5b48a8fe06
DayannaEspinal <dayannaespinal14@gmail.com>
2026-04-13 11:32:27 -0600

    correcciones para ghpages

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from "react-router";
-import { PokemonProvider } from './context/Context.jsx';
+import { BrowserRouter } from 'react-router-dom'
+import { PokemonProvider } from './context/Context.jsx'
 import './index.css'
 import App from './App.jsx'
 
